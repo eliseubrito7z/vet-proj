3de2b615d441a1d5411746e5b755e0179e0c1612
eliseubrito7z <eliseubrito776@gmail.com>
2023-08-11 14:11:49 -0300

    chore: styled-components added and basic config

---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${(props) => props.theme['gray-800']};
+    color: ${(props) => props.theme['gray-100']};
+    -webkit-font-smoothing: antialiased;
+  }
+  
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,12 @@
+export const defaultTheme = {
+  "white-100": "#FFFFFF",
+  "white-300": "#E7E7E7",
+
+  "gray-100": "#A0A1A3",
+  
+  "green-300": "#8ED7C6",
+  "green-600": "#18C29C",
+  "green-900": "#084236",
+
+  "red-500": "#AB222E"
+} as const
