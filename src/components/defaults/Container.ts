be6c7923344f36533f8247eb43838d09cabc6cec
eliseubrito7z <eliseubrito776@gmail.com>
2023-08-11 14:40:25 -0300

    feat: added interface to Container component

--- src/components/defaults/Container.ts
+++ src/components/defaults/Container.ts
@@ -1,8 +1,14 @@
 import styled from 'styled-components'
 
+interface ContainerProps {
+  flex?: boolean
+}
 
-export const Container = styled.div`
-  display: flex;
+export const Container = styled.div<ContainerProps>`
+  display: ${(props) => 
+    props.flex ? 'flex' : 'block'
+  };
+  
   max-width: 1440px;
 
   width: 100vw;
